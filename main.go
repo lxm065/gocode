a5689269a1eec2bb8ade3c96d8c48de24e88c489
xmliang <151488669@qq.com>
2023-09-17 07:47:47 +0800

    a

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	// "github.com/thinkerou/favicon"
 )
 
+func myHandler() gin.HandlerFunc {
+
+	return func(context *gin.Context) {
+		context.Set("usersession", "userid-1")
+		context.Next()
+		// context.Abort()
+	}
+}
+
 func main() {
+
+	//创建一个服务
 	r := gin.Default()
 	// r.Use(favicon.New("./1.ico"))
+
+	//加载静态页面
+	r.LoadHTMLGlob("./templates/*")
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -20,6 +37,27 @@ func main() {
 			"message": "hello world!",
 		})
 	})
+
+	r.GET("/index", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"msg": "这是go后台传来的数据",
+		})
+	})
+
+	r.GET("/user/info", myHandler(), func(context *gin.Context) {
+		//取出中间件中的值
+		usersession := context.MustGet("usersession").(string)
+		log.Println("++++++++++", usersession, "++++++++++")
+
+		userid := context.Query("userid")
+		username := context.Query("username")
+		context.JSON(http.StatusOK, gin.H{
+			"userid":   userid,
+			"username": username,
+		})
+
+	})
+
 	// r.Run() // listen and serve on 0.0.0.0:8080
 	r.Run(":9001")
 
